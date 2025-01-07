e89a1ed1b26e3c6844117c69e16ae8723016dbec
david19961129 <david19961129@gmail.com>
2025-01-07 16:15:51 +0800

    C#控制器

---
+++ Assets/Scripts/ControlSystem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControlSystem : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10f;
+
+    private Vector2 _moveInput;
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody=GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.linearVelocity =_moveInput*_speed;
+    }
+
+
+    private void onMove(InputValue value)
+    {
+        _moveInput=value.Get<Vector2>();
+    }
+}
