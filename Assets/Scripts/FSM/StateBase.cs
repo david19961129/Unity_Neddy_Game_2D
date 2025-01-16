7fbad477bc71ce19e1f71332371f9ae7998d011f
david19961129 <david19961129@gmail.com>
2025-01-16 16:25:42 +0800

    迷宮物理邊界已加上，剩小怪機制

---
+++ Assets/Scripts/FSM/StateBase.cs
@@ -0,0 +1,17 @@
+﻿using NEDDY.Tools;
+using UnityEngine;
+
+namespace NEDDY
+{
+    /// <summary>
+    /// 狀態基底
+    /// </summary>
+    public class StateBase:MonoBehaviour,IState
+    {
+        protected string stateName;
+        public virtual void EnterState()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Interface/IDamage.cs
@@ -0,0 +1,16 @@
+﻿namespace NEDDY
+{
+    interface IDamage
+    {
+        /// <summary>
+        /// 受傷
+        /// </summary>
+        /// <param name="damage"></param>
+        public void Damage(float damage);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dead();
+    }
+}
